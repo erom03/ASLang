857ef8569dbd320374339cb88d1911378c5f44e8
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-07 13:14:38 -0400

    Remove unneeded stuff

--- app/lessons/page.tsx
+++ app/lessons/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
 import Navigation from "../components/navigation";
 import GetOutput from "../components/get_output"
-import util from 'node:util';
-
-const exec = util.promisify(require('node:child_process').exec);
 
 const Lessons = () => {
   return (
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,6 @@
 import { signOut, useSession } from 'next-auth/react'
 import React from 'react';
 import Navigation from "../components/navigation";
-import util from 'node:util';
 
 const Login = () => {
   return (
