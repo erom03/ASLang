7582eb811a25aa93fe2f70746896351508d3ae17
bladex_3000 <aledie27@gmail.com>
2023-10-07 01:34:09 -0400

    small text fix

--- app/components/navigation/index.tsx
+++ app/components/navigation/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <>
       <div>
-        <div className="container my-1   max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
+        <div className="container my-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
           <div className="flex justify-between items-center h-full ml-5 mr-10">
             <Logo />
             <ul className="hidden md:flex gap-x-6 text-black">
