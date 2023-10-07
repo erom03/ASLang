dcb9efbe25ae02af51f72b37d09623179da2ac6b
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-07 02:00:40 -0400

    Reconsidering life choices

--- app/lessons/page.tsx
+++ app/lessons/page.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
 import Navigation from "../components/navigation";
+import util from 'node:util';
+
+const exec = util.promisify(require('node:child_process').exec);
 
 const page = () => {
   return (
     <section>
     <header><Navigation /></header>
     <div>lessons page here</div>
+    <div>{/*getOutput()*/}</div>
     </section>
   )
+}
+
+async function getOutput() {
+  let programOutput = "";
+
+  const { stdout } = await exec('/test.bat');
+
+  if(stdout) {
+    for await (const data of stdout) {
+      programOutput += data;
+    }
+  }
+
+  return programOutput;
 }
 
 export default page
