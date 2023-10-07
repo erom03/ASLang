fda813d48b90308f5b49a9126d4fa7897acf4b32
OatmealJester <drrr032@gmail.com>
2023-10-07 03:22:50 -0400

    Fixed Darkmode text bug

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       <main>
 
       <div className="max-w-4xl mx-auto sm:pt-16">
-        <h1 className="text-4xl sm:text-6xl text-gray-700 dark:text-gray-200 font-medium mx-auto pb-6 tracking-tighter">
+        <h1 className="text-4xl sm:text-6xl text-gray-700  font-medium mx-auto pb-6 tracking-tighter">
           Your
-          <span className="text-yellow-600 dark:text-yellow-600"> American Sign Language</span> 
+          <span className="text-yellow-600 "> American Sign Language</span> 
           <span className="block font-medium"> Journey Begins Here</span>
         </h1>
-        <div className="text-gray-600 dark:text-gray-300/80 pb-8 max-w-xl mx-auto">
-          <p className="mb-4 text-xl text-gray-500 dark:text-gray-400">Our American Sign Language curriculum is free and open source</p>
+        <div className="text-gray-600  pb-8 max-w-xl mx-auto">
+          <p className="mb-4 text-xl text-gray-500 ">Our American Sign Language curriculum is free and open source</p>
         </div>
       </div>
 
