14d95dcc358af6b4d8445975d214570edf7ab8bd
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-07 13:11:35 -0400

    trying to fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,15 @@
 'use client';
 
-import { signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react';
 import Navigation from "../components/navigation";
 import util from 'node:util';
-import GetOutput from '../components/get_output';
-
-const exec = util.promisify(require('node:child_process').exec);
 
 const Login = () => {
   return (
     <section>
     <header><Navigation /></header>
     <div>login page goes here</div>
-    <div>{GetOutput()}</div>
     </section>
   )
 }
