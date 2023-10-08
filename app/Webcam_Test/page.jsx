f90005db64eeec877a9cad20ee252d6892c16642
Enrique Romero <enrique.romero1110@gmail.com>
2023-10-08 01:19:48 -0400

    Webcam is going

--- app/Webcam_Test/page.jsx
+++ app/Webcam_Test/page.jsx
@@ -1,13 +1,33 @@
 "use client"
 
-import Webcam from "react-webcam";
+import Webcam from 'react-webcam';
 import React from 'react'
 
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
 const page = () => {
-  return(<div className="page">
-    React Webcam
-    <Webcam/>
-  </div>)
+  return(
+  <Webcam
+    audio={false}
+    height={720}
+    screenshotFormat="image/jpeg"
+    width={1280}
+    videoConstraints={videoConstraints}
+  >
+    {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot();
+        }}
+      >
+        Capture photo
+      </button>
+    )}
+  </Webcam>);
 }
 
 export default page
